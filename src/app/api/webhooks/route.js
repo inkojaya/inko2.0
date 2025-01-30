fc336948f99fcf8d03538b7ed4e4662855c2fe07
koridal <hjpark002@gmail.com>
2025-01-30 10:33:51 +0900

    Fix some error

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,6 +1,6 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+// import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
 import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
@@ -56,49 +56,49 @@ export async function POST(req) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
 
-  if (eventType === 'user.created' || eventType === 'user.updated') {
-    const { id, first_name, last_name, image_url, email_addresses, username } =
-      evt?.data;
-    try {
-      const user = await createOrUpdateUser(
-        id,
-        first_name,
-        last_name,
-        image_url,
-        email_addresses,
-        username
-      );
-      if (user && eventType === 'user.created') {
-        try {
-          await clerkClient.users.updateUserMetadata(id, {
-            publicMetadata: {
-              userMongoId: user._id,
-              isAdmin: user.isAdmin,
-            },
-          });
-        } catch (error) {
-          console.log('Error updating user metadata:', error);
-        }
-      }
-    } catch (error) {
-      console.log('Error creating or updating user:', error);
-      return new Response('Error occured', {
-        status: 400,
-      });
-    }
-  }
+  // if (eventType === 'user.created' || eventType === 'user.updated') {
+  //   const { id, first_name, last_name, image_url, email_addresses, username } =
+  //     evt?.data;
+  //   try {
+  //     const user = await createOrUpdateUser(
+  //       id,
+  //       first_name,
+  //       last_name,
+  //       image_url,
+  //       email_addresses,
+  //       username
+  //     );
+  //     if (user && eventType === 'user.created') {
+  //       try {
+  //         await clerkClient.users.updateUserMetadata(id, {
+  //           publicMetadata: {
+  //             userMongoId: user._id,
+  //             isAdmin: user.isAdmin,
+  //           },
+  //         });
+  //       } catch (error) {
+  //         console.log('Error updating user metadata:', error);
+  //       }
+  //     }
+  //   } catch (error) {
+  //     console.log('Error creating or updating user:', error);
+  //     return new Response('Error occured', {
+  //       status: 400,
+  //     });
+  //   }
+  // }
 
-  if (eventType === 'user.deleted') {
-    const { id } = evt?.data;
-    try {
-      await deleteUser(id);
-    } catch (error) {
-      console.log('Error deleting user:', error);
-      return new Response('Error occured', {
-        status: 400,
-      });
-    }
-  }
+  // if (eventType === 'user.deleted') {
+  //   const { id } = evt?.data;
+  //   try {
+  //     await deleteUser(id);
+  //   } catch (error) {
+  //     console.log('Error deleting user:', error);
+  //     return new Response('Error occured', {
+  //       status: 400,
+  //     });
+  //   }
+  // }
 
   return new Response('', { status: 200 });
 }
